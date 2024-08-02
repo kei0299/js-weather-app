dc3e56eb2bf87c6350bcc36cb19d73903e4c894d
kei0299 <kitagen_1026@yahoo.co.jp>
2024-08-03 01:39:27 +0900

    HTML作成まで

---
+++ main.js
@@ -0,0 +1,4 @@
+const weatherForm = document.getElementById("weather-form")
+const cityInput = document.getElementById("city-input")
+const weatherResults = document.getElementById("weather-results")
+
