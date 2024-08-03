901e037f407af02cb252135f2d6b89c9260a837f
kei0299 <kitagen_1026@yahoo.co.jp>
2024-08-03 17:55:21 +0900

    国名取得

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const weatherForm = document.getElementById("weather-form")
 const cityInput = document.getElementById("city-input")
 const weatherResults = document.getElementById("weather-results")
 
+fetch("https://api.weatherapi.com/v1/current.json?key=07e5ff9f1259439ebd6122540240208&q=kyoto&aqi=no")
+    .then(response=>response.json())
+    .then(jsonData => console.log(jsonData.location.country))
